177f69968ce859047cc53a8b68a0a209b71cdd23
Stefan Ziegler <edi.gonzales@gmail.com>
2024-08-12 10:24:40 +0200

    use base64 for logo

--- wgc-embedded-client/src/main/java/ch/so/agi/wgc/components/jumplink/JumpLinkComponent.java
+++ wgc-embedded-client/src/main/java/ch/so/agi/wgc/components/jumplink/JumpLinkComponent.java
@@ -28,11 +28,15 @@ public class JumpLinkComponent {
         stateManager = StateManager.getInstance();
         stateManager.subscribe(StateManager.PARAM_BROWSER_URL, (oldBrowserUrl, newBrowserUrl) -> onChangeBrowserUrl((String)newBrowserUrl));
 
+        // Es scheint, als ob der Name "favicon" irgendwo geblockt wird.
+        // Mit "fubar.png" funktioniert.
+        // Hier jetzt mit base64 gelöst.
         String imageUrl = "fubar.png";  
         String href = (String) stateManager.getState(StateManager.PARAM_BROWSER_URL);
         anchorElement = a().css("wgcLink").attr("href", href).attr("target", "_blank").add(span().textContent("In geo.so.ch/map ansehen")).element();
         root = div().css("wgcLinkContainer")
-                .add(img().attr("src", imageUrl).attr("width", "16"))
+                //.add(img().attr("src", imageUrl).attr("width", "16"))
+                .add(img().attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAFNAAABTQG7wV/dAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAgxJREFUWIXtlU9rGkEYh3+zWyeJkqYGZUHU6bG9pgRBxIMnbz0U+gV6DPTcQA81h5rPoKfeihRaFEygeLWI5OJFKAFBhFAsG/wzrFozby/boIkm0rrQw/5gD/O+wzzPDDsMIyIAAGPsZcTn+7AB6HAwI6WoY1lxIjoDgAczva1nuu552+87KvCac+oA/M9YcxK2SlwBV8AVcAVcAVdg9jUEj8flk6Ojh04C+cHBFPX6YgG2s6N8+/tO8qFtb6u5saO0FfJfCYwnk4laOnNNGY/HBEAuEmi2221yWsA0zSsArUUCLdM0HRVQSsGyrCkRDW4JEFFfSnnZbDYdEyiXy+Cct2drcz9hbzD48rFQ+OWUwMnpKQ2Hw09zRSK6/gA8FUJ0pZS07nQ6HRJCXALYnWVqN2Sapml+fpfJWOve/ZvDwytiLEdE5tITsE9hNxgMtkqlklrX7nO5HEUikXMAnlu8mwVbYi8UCl1Uq9V/hheLRRJC/AAQXchaVLQlUoZhtPP5/PRv4dnjYyWEuPB6vXtLOcsatsTjQCBwlk6ne/V6fWVwpVKhZDIpo9FoFYD/TsZdTVuCeTyeV4ZhnCcSCfN9NqsajQZZlnUNlFJSrVajTCajYrFYLxwOf+ecv7hvbSICsyH3hjGmAUg98vufb21upgDsaJq2AYARkaXres8ajb7+7HYLRPRt1dvxG3gh0UeKP+U/AAAAAElFTkSuQmCC").attr("width", "16"))
                 .add(span().add(anchorElement))
                 .element();
         
